dd6dce83ce0da89e0accdc09653670c7316028ea
rivergod <rivernile@naver.com>
2011-12-16 21:33:45 +0900

    Appending tools/CreateDB Project

---
+++ tools/CreateDB/js/moct.js
@@ -0,0 +1,949 @@
+var moctGu = {
+	"moctJsonList" : [ {
+		"GUGUN_NAME" : "강남구",
+		"GUGUN_CODE" : "11680"
+	}, {
+		"GUGUN_NAME" : "강동구",
+		"GUGUN_CODE" : "11740"
+	}, {
+		"GUGUN_NAME" : "강북구",
+		"GUGUN_CODE" : "11305"
+	}, {
+		"GUGUN_NAME" : "강서구",
+		"GUGUN_CODE" : "11500"
+	}, {
+		"GUGUN_NAME" : "관악구",
+		"GUGUN_CODE" : "11620"
+	}, {
+		"GUGUN_NAME" : "광진구",
+		"GUGUN_CODE" : "11215"
+	}, {
+		"GUGUN_NAME" : "구로구",
+		"GUGUN_CODE" : "11530"
+	}, {
+		"GUGUN_NAME" : "금천구",
+		"GUGUN_CODE" : "11545"
+	}, {
+		"GUGUN_NAME" : "노원구",
+		"GUGUN_CODE" : "11350"
+	}, {
+		"GUGUN_NAME" : "도봉구",
+		"GUGUN_CODE" : "11320"
+	}, {
+		"GUGUN_NAME" : "동대문구",
+		"GUGUN_CODE" : "11230"
+	}, {
+		"GUGUN_NAME" : "동작구",
+		"GUGUN_CODE" : "11590"
+	}, {
+		"GUGUN_NAME" : "마포구",
+		"GUGUN_CODE" : "11440"
+	}, {
+		"GUGUN_NAME" : "서대문구",
+		"GUGUN_CODE" : "11410"
+	}, {
+		"GUGUN_NAME" : "서초구",
+		"GUGUN_CODE" : "11650"
+	}, {
+		"GUGUN_NAME" : "성동구",
+		"GUGUN_CODE" : "11200"
+	}, {
+		"GUGUN_NAME" : "성북구",
+		"GUGUN_CODE" : "11290"
+	}, {
+		"GUGUN_NAME" : "송파구",
+		"GUGUN_CODE" : "11710"
+	}, {
+		"GUGUN_NAME" : "양천구",
+		"GUGUN_CODE" : "11470"
+	}, {
+		"GUGUN_NAME" : "영등포구",
+		"GUGUN_CODE" : "11560"
+	}, {
+		"GUGUN_NAME" : "용산구",
+		"GUGUN_CODE" : "11170"
+	}, {
+		"GUGUN_NAME" : "은평구",
+		"GUGUN_CODE" : "11380"
+	}, {
+		"GUGUN_NAME" : "종로구",
+		"GUGUN_CODE" : "11110"
+	}, {
+		"GUGUN_NAME" : "중구",
+		"GUGUN_CODE" : "11140"
+	}, {
+		"GUGUN_NAME" : "중랑구",
+		"GUGUN_CODE" : "11260"
+	} ]
+};
+
+var moctDong = {
+	"moctJsonList" : [ {
+		"DONG_CODE" : "1130510100",
+		"DONG_NAME" : "미아동"
+	}, {
+		"DONG_CODE" : "1130510200",
+		"DONG_NAME" : "번동"
+	}, {
+		"DONG_CODE" : "1130510300",
+		"DONG_NAME" : "수유동"
+	}, {
+		"DONG_CODE" : "1130510400",
+		"DONG_NAME" : "우이동"
+	}, {
+		"DONG_CODE" : "1121510400",
+		"DONG_NAME" : "광장동"
+	}, {
+		"DONG_CODE" : "1121510300",
+		"DONG_NAME" : "구의동"
+	}, {
+		"DONG_CODE" : "1121510900",
+		"DONG_NAME" : "군자동"
+	}, {
+		"DONG_CODE" : "1121510200",
+		"DONG_NAME" : "능동"
+	}, {
+		"DONG_CODE" : "1121510500",
+		"DONG_NAME" : "자양동"
+	}, {
+		"DONG_CODE" : "1121510100",
+		"DONG_NAME" : "중곡동"
+	}, {
+		"DONG_CODE" : "1121510700",
+		"DONG_NAME" : "화양동"
+	}, {
+		"DONG_CODE" : "1168010300",
+		"DONG_NAME" : "개포동"
+	}, {
+		"DONG_CODE" : "1168010800",
+		"DONG_NAME" : "논현동"
+	}, {
+		"DONG_CODE" : "1168010600",
+		"DONG_NAME" : "대치동"
+	}, {
+		"DONG_CODE" : "1168011800",
+		"DONG_NAME" : "도곡동"
+	}, {
+		"DONG_CODE" : "1168010500",
+		"DONG_NAME" : "삼성동"
+	}, {
+		"DONG_CODE" : "1168011100",
+		"DONG_NAME" : "세곡동"
+	}, {
+		"DONG_CODE" : "1168011500",
+		"DONG_NAME" : "수서동"
+	}, {
+		"DONG_CODE" : "1168010700",
+		"DONG_NAME" : "신사동"
+	}, {
+		"DONG_CODE" : "1168011000",
+		"DONG_NAME" : "압구정동"
+	}, {
+		"DONG_CODE" : "1168010100",
+		"DONG_NAME" : "역삼동"
+	}, {
+		"DONG_CODE" : "1168011400",
+		"DONG_NAME" : "일원동"
+	}, {
+		"DONG_CODE" : "1168010400",
+		"DONG_NAME" : "청담동"
+	}, {
+		"DONG_CODE" : "1123010500",
+		"DONG_NAME" : "답십리동"
+	}, {
+		"DONG_CODE" : "1123010100",
+		"DONG_NAME" : "신설동"
+	}, {
+		"DONG_CODE" : "1123010200",
+		"DONG_NAME" : "용두동"
+	}, {
+		"DONG_CODE" : "1123011000",
+		"DONG_NAME" : "이문동"
+	}, {
+		"DONG_CODE" : "1123010600",
+		"DONG_NAME" : "장안동"
+	}, {
+		"DONG_CODE" : "1123010400",
+		"DONG_NAME" : "전농동"
+	}, {
+		"DONG_CODE" : "1123010300",
+		"DONG_NAME" : "제기동"
+	}, {
+		"DONG_CODE" : "1123010700",
+		"DONG_NAME" : "청량리동"
+	}, {
+		"DONG_CODE" : "1123010800",
+		"DONG_NAME" : "회기동"
+	}, {
+		"DONG_CODE" : "1123010900",
+		"DONG_NAME" : "휘경동"
+	}, {
+		"DONG_CODE" : "1153010300",
+		"DONG_NAME" : "가리봉동"
+	}, {
+		"DONG_CODE" : "1153010700",
+		"DONG_NAME" : "개봉동"
+	}, {
+		"DONG_CODE" : "1153010600",
+		"DONG_NAME" : "고척동"
+	}, {
+		"DONG_CODE" : "1153010200",
+		"DONG_NAME" : "구로동"
+	}, {
+		"DONG_CODE" : "1153010900",
+		"DONG_NAME" : "궁동"
+	}, {
+		"DONG_CODE" : "1153010100",
+		"DONG_NAME" : "신도림동"
+	}, {
+		"DONG_CODE" : "1153010800",
+		"DONG_NAME" : "오류동"
+	}, {
+		"DONG_CODE" : "1153011000",
+		"DONG_NAME" : "온수동"
+	}, {
+		"DONG_CODE" : "1153011100",
+		"DONG_NAME" : "천왕동"
+	}, {
+		"DONG_CODE" : "1153011200",
+		"DONG_NAME" : "항동"
+	}, {
+		"DONG_CODE" : "1147010200",
+		"DONG_NAME" : "목동"
+	}, {
+		"DONG_CODE" : "1147010300",
+		"DONG_NAME" : "신월동"
+	}, {
+		"DONG_CODE" : "1147010100",
+		"DONG_NAME" : "신정동"
+	}, {
+		"DONG_CODE" : "1117010400",
+		"DONG_NAME" : "갈월동"
+	}, {
+		"DONG_CODE" : "1117012000",
+		"DONG_NAME" : "도원동"
+	}, {
+		"DONG_CODE" : "1117012200",
+		"DONG_NAME" : "문배동"
+	}, {
+		"DONG_CODE" : "1117013600",
+		"DONG_NAME" : "보광동"
+	}, {
+		"DONG_CODE" : "1117011500",
+		"DONG_NAME" : "산천동"
+	}, {
+		"DONG_CODE" : "1117013300",
+		"DONG_NAME" : "서빙고동"
+	}, {
+		"DONG_CODE" : "1117012300",
+		"DONG_NAME" : "신계동"
+	}, {
+		"DONG_CODE" : "1117011400",
+		"DONG_NAME" : "신창동"
+	}, {
+		"DONG_CODE" : "1117012100",
+		"DONG_NAME" : "용문동"
+	}, {
+		"DONG_CODE" : "1117010200",
+		"DONG_NAME" : "용산동2가"
+	}, {
+		"DONG_CODE" : "1117012700",
+		"DONG_NAME" : "용산동5가"
+	}, {
+		"DONG_CODE" : "1117011200",
+		"DONG_NAME" : "원효로1가"
+	}, {
+		"DONG_CODE" : "1117011300",
+		"DONG_NAME" : "원효로2가"
+	}, {
+		"DONG_CODE" : "1117011700",
+		"DONG_NAME" : "원효로3가"
+	}, {
+		"DONG_CODE" : "1117011800",
+		"DONG_NAME" : "원효로4가"
+	}, {
+		"DONG_CODE" : "1117012900",
+		"DONG_NAME" : "이촌동"
+	}, {
+		"DONG_CODE" : "1117013000",
+		"DONG_NAME" : "이태원동"
+	}, {
+		"DONG_CODE" : "1117011600",
+		"DONG_NAME" : "청암동"
+	}, {
+		"DONG_CODE" : "1117011100",
+		"DONG_NAME" : "청파동3가"
+	}, {
+		"DONG_CODE" : "1117012400",
+		"DONG_NAME" : "한강로1가"
+	}, {
+		"DONG_CODE" : "1117012500",
+		"DONG_NAME" : "한강로2가"
+	}, {
+		"DONG_CODE" : "1117012800",
+		"DONG_NAME" : "한강로3가"
+	}, {
+		"DONG_CODE" : "1117013100",
+		"DONG_NAME" : "한남동"
+	}, {
+		"DONG_CODE" : "1117011900",
+		"DONG_NAME" : "효창동"
+	}, {
+		"DONG_CODE" : "1117010100",
+		"DONG_NAME" : "후암동"
+	}, {
+		"DONG_CODE" : "1120010900",
+		"DONG_NAME" : "금호동1가"
+	}, {
+		"DONG_CODE" : "1120011000",
+		"DONG_NAME" : "금호동2가"
+	}, {
+		"DONG_CODE" : "1120011100",
+		"DONG_NAME" : "금호동3가"
+	}, {
+		"DONG_CODE" : "1120011200",
+		"DONG_NAME" : "금호동4가"
+	}, {
+		"DONG_CODE" : "1120010400",
+		"DONG_NAME" : "도선동"
+	}, {
+		"DONG_CODE" : "1120010500",
+		"DONG_NAME" : "마장동"
+	}, {
+		"DONG_CODE" : "1120010600",
+		"DONG_NAME" : "사근동"
+	}, {
+		"DONG_CODE" : "1120011400",
+		"DONG_NAME" : "성수동1가"
+	}, {
+		"DONG_CODE" : "1120011500",
+		"DONG_NAME" : "성수동2가"
+	}, {
+		"DONG_CODE" : "1120011800",
+		"DONG_NAME" : "송정동"
+	}, {
+		"DONG_CODE" : "1120011300",
+		"DONG_NAME" : "옥수동"
+	}, {
+		"DONG_CODE" : "1120012200",
+		"DONG_NAME" : "용답동"
+	}, {
+		"DONG_CODE" : "1120010800",
+		"DONG_NAME" : "응봉동"
+	}, {
+		"DONG_CODE" : "1120010200",
+		"DONG_NAME" : "하왕십리동"
+	}, {
+		"DONG_CODE" : "1120010700",
+		"DONG_NAME" : "행당동"
+	}, {
+		"DONG_CODE" : "1120010300",
+		"DONG_NAME" : "홍익동"
+	}, {
+		"DONG_CODE" : "1156011700",
+		"DONG_NAME" : "당산동"
+	}, {
+		"DONG_CODE" : "1156011100",
+		"DONG_NAME" : "당산동1가"
+	}, {
+		"DONG_CODE" : "1156011200",
+		"DONG_NAME" : "당산동2가"
+	}, {
+		"DONG_CODE" : "1156011300",
+		"DONG_NAME" : "당산동3가"
+	}, {
+		"DONG_CODE" : "1156011400",
+		"DONG_NAME" : "당산동4가"
+	}, {
+		"DONG_CODE" : "1156011500",
+		"DONG_NAME" : "당산동5가"
+	}, {
+		"DONG_CODE" : "1156013300",
+		"DONG_NAME" : "대림동"
+	}, {
+		"DONG_CODE" : "1156011800",
+		"DONG_NAME" : "도림동"
+	}, {
+		"DONG_CODE" : "1156012000",
+		"DONG_NAME" : "문래동2가"
+	}, {
+		"DONG_CODE" : "1156012100",
+		"DONG_NAME" : "문래동3가"
+	}, {
+		"DONG_CODE" : "1156012200",
+		"DONG_NAME" : "문래동4가"
+	}, {
+		"DONG_CODE" : "1156012300",
+		"DONG_NAME" : "문래동5가"
+	}, {
+		"DONG_CODE" : "1156012400",
+		"DONG_NAME" : "문래동6가"
+	}, {
+		"DONG_CODE" : "1156013200",
+		"DONG_NAME" : "신길동"
+	}, {
+		"DONG_CODE" : "1156012500",
+		"DONG_NAME" : "양평동1가"
+	}, {
+		"DONG_CODE" : "1156012600",
+		"DONG_NAME" : "양평동2가"
+	}, {
+		"DONG_CODE" : "1156012700",
+		"DONG_NAME" : "양평동3가"
+	}, {
+		"DONG_CODE" : "1156012800",
+		"DONG_NAME" : "양평동4가"
+	}, {
+		"DONG_CODE" : "1156012900",
+		"DONG_NAME" : "양평동5가"
+	}, {
+		"DONG_CODE" : "1156013000",
+		"DONG_NAME" : "양평동6가"
+	}, {
+		"DONG_CODE" : "1156011000",
+		"DONG_NAME" : "여의도동"
+	}, {
+		"DONG_CODE" : "1156010100",
+		"DONG_NAME" : "영등포동"
+	}, {
+		"DONG_CODE" : "1156010200",
+		"DONG_NAME" : "영등포동1가"
+	}, {
+		"DONG_CODE" : "1156010600",
+		"DONG_NAME" : "영등포동5가"
+	}, {
+		"DONG_CODE" : "1156010800",
+		"DONG_NAME" : "영등포동7가"
+	}, {
+		"DONG_CODE" : "1156010900",
+		"DONG_NAME" : "영등포동8가"
+	}, {
+		"DONG_CODE" : "1132010800",
+		"DONG_NAME" : "도봉동"
+	}, {
+		"DONG_CODE" : "1132010600",
+		"DONG_NAME" : "방학동"
+	}, {
+		"DONG_CODE" : "1132010500",
+		"DONG_NAME" : "쌍문동"
+	}, {
+		"DONG_CODE" : "1132010700",
+		"DONG_NAME" : "창동"
+	}, {
+		"DONG_CODE" : "1165010700",
+		"DONG_NAME" : "반포동"
+	}, {
+		"DONG_CODE" : "1165010100",
+		"DONG_NAME" : "방배동"
+	}, {
+		"DONG_CODE" : "1165010800",
+		"DONG_NAME" : "서초동"
+	}, {
+		"DONG_CODE" : "1165010200",
+		"DONG_NAME" : "양재동"
+	}, {
+		"DONG_CODE" : "1165010300",
+		"DONG_NAME" : "우면동"
+	}, {
+		"DONG_CODE" : "1165010600",
+		"DONG_NAME" : "잠원동"
+	}, {
+		"DONG_CODE" : "1129013400",
+		"DONG_NAME" : "길음동"
+	}, {
+		"DONG_CODE" : "1129010300",
+		"DONG_NAME" : "돈암동"
+	}, {
+		"DONG_CODE" : "1129011900",
+		"DONG_NAME" : "동선동4가"
+	}, {
+		"DONG_CODE" : "1129010700",
+		"DONG_NAME" : "동소문동4가"
+	}, {
+		"DONG_CODE" : "1129010800",
+		"DONG_NAME" : "동소문동5가"
+	}, {
+		"DONG_CODE" : "1129010900",
+		"DONG_NAME" : "동소문동6가"
+	}, {
+		"DONG_CODE" : "1129011000",
+		"DONG_NAME" : "동소문동7가"
+	}, {
+		"DONG_CODE" : "1129013200",
+		"DONG_NAME" : "보문동3가"
+	}, {
+		"DONG_CODE" : "1129012800",
+		"DONG_NAME" : "보문동6가"
+	}, {
+		"DONG_CODE" : "1129011200",
+		"DONG_NAME" : "삼선동2가"
+	}, {
+		"DONG_CODE" : "1129011300",
+		"DONG_NAME" : "삼선동3가"
+	}, {
+		"DONG_CODE" : "1129011400",
+		"DONG_NAME" : "삼선동4가"
+	}, {
+		"DONG_CODE" : "1129013700",
+		"DONG_NAME" : "상월곡동"
+	}, {
+		"DONG_CODE" : "1129013900",
+		"DONG_NAME" : "석관동"
+	}, {
+		"DONG_CODE" : "1129010100",
+		"DONG_NAME" : "성북동"
+	}, {
+		"DONG_CODE" : "1129012100",
+		"DONG_NAME" : "안암동1가"
+	}, {
+		"DONG_CODE" : "1129012300",
+		"DONG_NAME" : "안암동3가"
+	}, {
+		"DONG_CODE" : "1129012400",
+		"DONG_NAME" : "안암동4가"
+	}, {
+		"DONG_CODE" : "1129013800",
+		"DONG_NAME" : "장위동"
+	}, {
+		"DONG_CODE" : "1129013300",
+		"DONG_NAME" : "정릉동"
+	}, {
+		"DONG_CODE" : "1129013500",
+		"DONG_NAME" : "종암동"
+	}, {
+		"DONG_CODE" : "1129013600",
+		"DONG_NAME" : "하월곡동"
+	}, {
+		"DONG_CODE" : "1171010700",
+		"DONG_NAME" : "가락동"
+	}, {
+		"DONG_CODE" : "1171011300",
+		"DONG_NAME" : "거여동"
+	}, {
+		"DONG_CODE" : "1171011400",
+		"DONG_NAME" : "마천동"
+	}, {
+		"DONG_CODE" : "1171010800",
+		"DONG_NAME" : "문정동"
+	}, {
+		"DONG_CODE" : "1171011100",
+		"DONG_NAME" : "방이동"
+	}, {
+		"DONG_CODE" : "1171010600",
+		"DONG_NAME" : "삼전동"
+	}, {
+		"DONG_CODE" : "1171010500",
+		"DONG_NAME" : "석촌동"
+	}, {
+		"DONG_CODE" : "1171010400",
+		"DONG_NAME" : "송파동"
+	}, {
+		"DONG_CODE" : "1171010200",
+		"DONG_NAME" : "신천동"
+	}, {
+		"DONG_CODE" : "1171011200",
+		"DONG_NAME" : "오금동"
+	}, {
+		"DONG_CODE" : "1171010100",
+		"DONG_NAME" : "잠실동"
+	}, {
+		"DONG_CODE" : "1171010900",
+		"DONG_NAME" : "장지동"
+	}, {
+		"DONG_CODE" : "1171010300",
+		"DONG_NAME" : "풍납동"
+	}, {
+		"DONG_CODE" : "1154510100",
+		"DONG_NAME" : "가산동"
+	}, {
+		"DONG_CODE" : "1154510200",
+		"DONG_NAME" : "독산동"
+	}, {
+		"DONG_CODE" : "1154510300",
+		"DONG_NAME" : "시흥동"
+	}, {
+		"DONG_CODE" : "1162010300",
+		"DONG_NAME" : "남현동"
+	}, {
+		"DONG_CODE" : "1162010100",
+		"DONG_NAME" : "봉천동"
+	}, {
+		"DONG_CODE" : "1162010200",
+		"DONG_NAME" : "신림동"
+	}, {
+		"DONG_CODE" : "1141012000",
+		"DONG_NAME" : "남가좌동"
+	}, {
+		"DONG_CODE" : "1141010500",
+		"DONG_NAME" : "냉천동"
+	}, {
+		"DONG_CODE" : "1141011200",
+		"DONG_NAME" : "대현동"
+	}, {
+		"DONG_CODE" : "1141010400",
+		"DONG_NAME" : "미근동"
+	}, {
+		"DONG_CODE" : "1141011900",
+		"DONG_NAME" : "북가좌동"
+	}, {
+		"DONG_CODE" : "1141011000",
+		"DONG_NAME" : "북아현동"
+	}, {
+		"DONG_CODE" : "1141011700",
+		"DONG_NAME" : "연희동"
+	}, {
+		"DONG_CODE" : "1141010800",
+		"DONG_NAME" : "영천동"
+	}, {
+		"DONG_CODE" : "1141011600",
+		"DONG_NAME" : "창천동"
+	}, {
+		"DONG_CODE" : "1141010600",
+		"DONG_NAME" : "천연동"
+	}, {
+		"DONG_CODE" : "1141010100",
+		"DONG_NAME" : "충정로2가"
+	}, {
+		"DONG_CODE" : "1141010200",
+		"DONG_NAME" : "충정로3가"
+	}, {
+		"DONG_CODE" : "1141010300",
+		"DONG_NAME" : "합동"
+	}, {
+		"DONG_CODE" : "1141010900",
+		"DONG_NAME" : "현저동"
+	}, {
+		"DONG_CODE" : "1141011800",
+		"DONG_NAME" : "홍은동"
+	}, {
+		"DONG_CODE" : "1141011100",
+		"DONG_NAME" : "홍제동"
+	}, {
+		"DONG_CODE" : "1174011000",
+		"DONG_NAME" : "강일동"
+	}, {
+		"DONG_CODE" : "1174010200",
+		"DONG_NAME" : "고덕동"
+	}, {
+		"DONG_CODE" : "1174010500",
+		"DONG_NAME" : "길동"
+	}, {
+		"DONG_CODE" : "1174010600",
+		"DONG_NAME" : "둔촌동"
+	}, {
+		"DONG_CODE" : "1174010100",
+		"DONG_NAME" : "명일동"
+	}, {
+		"DONG_CODE" : "1174010300",
+		"DONG_NAME" : "상일동"
+	}, {
+		"DONG_CODE" : "1174010800",
+		"DONG_NAME" : "성내동"
+	}, {
+		"DONG_CODE" : "1174010700",
+		"DONG_NAME" : "암사동"
+	}, {
+		"DONG_CODE" : "1174010900",
+		"DONG_NAME" : "천호동"
+	}, {
+		"DONG_CODE" : "1150010400",
+		"DONG_NAME" : "가양동"
+	}, {
+		"DONG_CODE" : "1150010800",
+		"DONG_NAME" : "공항동"
+	}, {
+		"DONG_CODE" : "1150010600",
+		"DONG_NAME" : "내발산동"
+	}, {
+		"DONG_CODE" : "1150010200",
+		"DONG_NAME" : "등촌동"
+	}, {
+		"DONG_CODE" : "1150010500",
+		"DONG_NAME" : "마곡동"
+	}, {
+		"DONG_CODE" : "1150010900",
+		"DONG_NAME" : "방화동"
+	}, {
+		"DONG_CODE" : "1150010100",
+		"DONG_NAME" : "염창동"
+	}, {
+		"DONG_CODE" : "1150010300",
+		"DONG_NAME" : "화곡동"
+	}, {
+		"DONG_CODE" : "1159010100",
+		"DONG_NAME" : "노량진동"
+	}, {
+		"DONG_CODE" : "1159010800",
+		"DONG_NAME" : "대방동"
+	}, {
+		"DONG_CODE" : "1159010600",
+		"DONG_NAME" : "동작동"
+	}, {
+		"DONG_CODE" : "1159010400",
+		"DONG_NAME" : "본동"
+	}, {
+		"DONG_CODE" : "1159010700",
+		"DONG_NAME" : "사당동"
+	}, {
+		"DONG_CODE" : "1159010300",
+		"DONG_NAME" : "상도1동"
+	}, {
+		"DONG_CODE" : "1159010200",
+		"DONG_NAME" : "상도동"
+	}, {
+		"DONG_CODE" : "1159010900",
+		"DONG_NAME" : "신대방동"
+	}, {
+		"DONG_CODE" : "1159010500",
+		"DONG_NAME" : "흑석동"
+	}, {
+		"DONG_CODE" : "1135010300",
+		"DONG_NAME" : "공릉동"
+	}, {
+		"DONG_CODE" : "1135010500",
+		"DONG_NAME" : "상계동"
+	}, {
+		"DONG_CODE" : "1135010200",
+		"DONG_NAME" : "월계동"
+	}, {
+		"DONG_CODE" : "1135010600",
+		"DONG_NAME" : "중계동"
+	}, {
+		"DONG_CODE" : "1135010400",
+		"DONG_NAME" : "하계동"
+	}, {
+		"DONG_CODE" : "1144010200",
+		"DONG_NAME" : "공덕동"
+	}, {
+		"DONG_CODE" : "1144011000",
+		"DONG_NAME" : "노고산동"
+	}, {
+		"DONG_CODE" : "1144010800",
+		"DONG_NAME" : "대흥동"
+	}, {
+		"DONG_CODE" : "1144010400",
+		"DONG_NAME" : "도화동"
+	}, {
+		"DONG_CODE" : "1144012100",
+		"DONG_NAME" : "동교동"
+	}, {
+		"DONG_CODE" : "1144010700",
+		"DONG_NAME" : "마포동"
+	}, {
+		"DONG_CODE" : "1144012300",
+		"DONG_NAME" : "망원동"
+	}, {
+		"DONG_CODE" : "1144011500",
+		"DONG_NAME" : "상수동"
+	}, {
+		"DONG_CODE" : "1144012700",
+		"DONG_NAME" : "상암동"
+	}, {
+		"DONG_CODE" : "1144012000",
+		"DONG_NAME" : "서교동"
+	}, {
+		"DONG_CODE" : "1144012500",
+		"DONG_NAME" : "성산동"
+	}, {
+		"DONG_CODE" : "1144010300",
+		"DONG_NAME" : "신공덕동"
+	}, {
+		"DONG_CODE" : "1144011100",
+		"DONG_NAME" : "신수동"
+	}, {
+		"DONG_CODE" : "1144011700",
+		"DONG_NAME" : "신정동"
+	}, {
+		"DONG_CODE" : "1144010100",
+		"DONG_NAME" : "아현동"
+	}, {
+		"DONG_CODE" : "1144012400",
+		"DONG_NAME" : "연남동"
+	}, {
+		"DONG_CODE" : "1144010900",
+		"DONG_NAME" : "염리동"
+	}, {
+		"DONG_CODE" : "1144010500",
+		"DONG_NAME" : "용강동"
+	}, {
+		"DONG_CODE" : "1144012600",
+		"DONG_NAME" : "중동"
+	}, {
+		"DONG_CODE" : "1144011400",
+		"DONG_NAME" : "창전동"
+	}, {
+		"DONG_CODE" : "1144010600",
+		"DONG_NAME" : "토정동"
+	}, {
+		"DONG_CODE" : "1144011600",
+		"DONG_NAME" : "하중동"
+	}, {
+		"DONG_CODE" : "1144012200",
+		"DONG_NAME" : "합정동"
+	}, {
+		"DONG_CODE" : "1144011200",
+		"DONG_NAME" : "현석동"
+	}, {
+		"DONG_CODE" : "1111012900",
+		"DONG_NAME" : "견지동"
+	}, {
+		"DONG_CODE" : "1111018000",
+		"DONG_NAME" : "교북동"
+	}, {
+		"DONG_CODE" : "1111018200",
+		"DONG_NAME" : "구기동"
+	}, {
+		"DONG_CODE" : "1111013700",
+		"DONG_NAME" : "낙원동"
+	}, {
+		"DONG_CODE" : "1111011800",
+		"DONG_NAME" : "내수동"
+	}, {
+		"DONG_CODE" : "1111011700",
+		"DONG_NAME" : "당주동"
+	}, {
+		"DONG_CODE" : "1111016800",
+		"DONG_NAME" : "동숭동"
+	}, {
+		"DONG_CODE" : "1111017000",
+		"DONG_NAME" : "명륜1가"
+	}, {
+		"DONG_CODE" : "1111017100",
+		"DONG_NAME" : "명륜2가"
+	}, {
+		"DONG_CODE" : "1111018700",
+		"DONG_NAME" : "무악동"
+	}, {
+		"DONG_CODE" : "1111018400",
+		"DONG_NAME" : "부암동"
+	}, {
+		"DONG_CODE" : "1111011500",
+		"DONG_NAME" : "사직동"
+	}, {
+		"DONG_CODE" : "1111017500",
+		"DONG_NAME" : "숭인동"
+	}, {
+		"DONG_CODE" : "1111018600",
+		"DONG_NAME" : "신영동"
+	}, {
+		"DONG_CODE" : "1111016000",
+		"DONG_NAME" : "연지동"
+	}, {
+		"DONG_CODE" : "1111016500",
+		"DONG_NAME" : "이화동"
+	}, {
+		"DONG_CODE" : "1111013300",
+		"DONG_NAME" : "익선동"
+	}, {
+		"DONG_CODE" : "1111015700",
+		"DONG_NAME" : "인의동"
+	}, {
+		"DONG_CODE" : "1111017400",
+		"DONG_NAME" : "창신동"
+	}, {
+		"DONG_CODE" : "1111018300",
+		"DONG_NAME" : "평창동"
+	}, {
+		"DONG_CODE" : "1111011300",
+		"DONG_NAME" : "필운동"
+	}, {
+		"DONG_CODE" : "1138010400",
+		"DONG_NAME" : "갈현동"
+	}, {
+		"DONG_CODE" : "1138010500",
+		"DONG_NAME" : "구산동"
+	}, {
+		"DONG_CODE" : "1138010200",
+		"DONG_NAME" : "녹번동"
+	}, {
+		"DONG_CODE" : "1138010600",
+		"DONG_NAME" : "대조동"
+	}, {
+		"DONG_CODE" : "1138010300",
+		"DONG_NAME" : "불광동"
+	}, {
+		"DONG_CODE" : "1138010100",
+		"DONG_NAME" : "수색동"
+	}, {
+		"DONG_CODE" : "1138010900",
+		"DONG_NAME" : "신사동"
+	}, {
+		"DONG_CODE" : "1138010800",
+		"DONG_NAME" : "역촌동"
+	}, {
+		"DONG_CODE" : "1138010700",
+		"DONG_NAME" : "응암동"
+	}, {
+		"DONG_CODE" : "1138011000",
+		"DONG_NAME" : "증산동"
+	}, {
+		"DONG_CODE" : "1138011400",
+		"DONG_NAME" : "진관동"
+	}, {
+		"DONG_CODE" : "1114011800",
+		"DONG_NAME" : "남대문로5가"
+	}, {
+		"DONG_CODE" : "1114011200",
+		"DONG_NAME" : "남창동"
+	}, {
+		"DONG_CODE" : "1114017300",
+		"DONG_NAME" : "만리동1가"
+	}, {
+		"DONG_CODE" : "1114017400",
+		"DONG_NAME" : "만리동2가"
+	}, {
+		"DONG_CODE" : "1114013600",
+		"DONG_NAME" : "묵정동"
+	}, {
+		"DONG_CODE" : "1114016800",
+		"DONG_NAME" : "순화동"
+	}, {
+		"DONG_CODE" : "1114016200",
+		"DONG_NAME" : "신당동"
+	}, {
+		"DONG_CODE" : "1114015100",
+		"DONG_NAME" : "을지로5가"
+	}, {
+		"DONG_CODE" : "1114013400",
+		"DONG_NAME" : "인현동2가"
+	}, {
+		"DONG_CODE" : "1114014400",
+		"DONG_NAME" : "장충동2가"
+	}, {
+		"DONG_CODE" : "1114017100",
+		"DONG_NAME" : "중림동"
+	}, {
+		"DONG_CODE" : "1114013200",
+		"DONG_NAME" : "충무로4가"
+	}, {
+		"DONG_CODE" : "1114016500",
+		"DONG_NAME" : "황학동"
+	}, {
+		"DONG_CODE" : "1114012100",
+		"DONG_NAME" : "회현동1가"
+	}, {
+		"DONG_CODE" : "1114012200",
+		"DONG_NAME" : "회현동2가"
+	}, {
+		"DONG_CODE" : "1114016300",
+		"DONG_NAME" : "흥인동"
+	}, {
+		"DONG_CODE" : "1126010500",
+		"DONG_NAME" : "망우동"
+	}, {
+		"DONG_CODE" : "1126010100",
+		"DONG_NAME" : "면목동"
+	}, {
+		"DONG_CODE" : "1126010400",
+		"DONG_NAME" : "묵동"
+	}, {
+		"DONG_CODE" : "1126010200",
+		"DONG_NAME" : "상봉동"
+	}, {
+		"DONG_CODE" : "1126010600",
+		"DONG_NAME" : "신내동"
+	}, {
+		"DONG_CODE" : "1126010300",
+		"DONG_NAME" : "중화동"
+	} ]
+};
